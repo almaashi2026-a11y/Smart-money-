60d366c00fb31e3e4f5cd1efa0124a6c032f5e64
almaashi2026-a11y <almaashi2026@gmail.com>
2026-06-23 01:03:08 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# إعدادات البوت والبيانات الحساسة
+TOKEN = '8757409287:AAGbCqnG8dEb2fxX9zodc7_QPLiil9MY1Rw'
+CHAT_ID = 'ضع_رقم_الـ_ID_هنا' # استبدله برقم الـ ID الخاص بك
+
+# إعدادات الفلترة العامة
+MIN_PRICE = 0.20
+MAX_PRICE = 10.00
+RVOL_THRESHOLD = 2.5
+
